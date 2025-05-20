458c8c7d57e269250dcb5184660999072d7fbaf6
miinamaaher1 <minamaher1515@gmail.com>
2025-05-20 04:30:24 +0300

    [Feature] added user service

---
+++ follow-service/FollowService/Services/Implementations/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Infrastructure.Repositories.Interfaces;
+using Services.Interfaces;
+
+namespace Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddUser(string id)
+        {
+            var user = await _unitOfWork.Users.FindAsync(u => u.Id == id);
+            if (user == null)
+            {
+                await _unitOfWork.Users.AddAsync(new User { Id = id });
+                await _unitOfWork.SaveAsync();
+            }
+        }
+
+        public async Task DeleteUser(string id)
+        {
+            var user = await _unitOfWork.Users.FindAsync(u => u.Id == id);
+            if (user != null)
+            {
+                _unitOfWork.Users.Delete(user);
+                await _unitOfWork.SaveAsync();
+            }
+        }
+    }
+}
---
+++ follow-service/FollowService/Services/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task AddUser(string id);
+        Task DeleteUser(string id);
+    }
+}
