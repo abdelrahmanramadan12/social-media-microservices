c34ce10cc11ccbed25023045bd295709fe1c7924
Mahmoud R. Keshk <mahmoudredakeshk@gmail.com>
2025-05-22 12:55:08 +0300

    [Project Setup] Add the presentation layer (API) and injectthe post service into it and assign the needed methods with each endpoint

---
+++ post-service/PostService/Presentation/Controllers/Public/PostsController.cs
@@ -0,0 +1,135 @@
+using Application.DTOs;
+using Application.IServices;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Presentation.Controllers.Public
+{
+    [Route("api/public/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostService _postService;
+        private readonly IEncryptionService _encryptionService;
+        public PostsController(IPostService postSerive, IEncryptionService encryptionService)
+        {
+            this._postService = postSerive;
+            this._encryptionService = encryptionService;
+        }
+
+
+        // Endpoints
+
+        [HttpGet("{postId}")]
+        public async Task<IActionResult> GetPost(string postId, [FromHeader(Name = "userId")] string userId)
+        {
+            ServiceResponse<PostResponseDTO> res = await _postService.GetPostByIdAsync(userId, postId);
+            if (!res.IsValid)
+            {
+                return HandleServiceError(res);
+            }
+            return Ok(new { data = res.DataItem });
+        }
+
+        [HttpGet("user/{profileUserId}")]
+        public async Task<IActionResult> GetProfilePostList(string profileUserId, [FromHeader(Name = "userId")] string userId, [FromQuery] string? next)
+        {
+            // Encrypt the Key if exists
+            if (!string.IsNullOrWhiteSpace(next))
+                next = _encryptionService.Decrypt(next);
+
+            const int pageSize = 15 + 1;
+            ServiceResponse<PostResponseDTO> res = await _postService.GetProfilePostListAsync(userId ,profileUserId, pageSize, next);
+            
+            if (!res.IsValid)
+                return HandleServiceError(res);
+
+            if (res.DataList?.Count >= pageSize)
+            {
+                var lastPost = res.DataList[^1];
+                res.DataList.RemoveAt(res.DataList.Count - 1);
+                string nextPostIdEncrpted = _encryptionService.Encrypt(lastPost.PostId);
+                return Ok(new { data = res.DataList , next = nextPostIdEncrpted});                       
+            }
+
+            return Ok(new { data = res.DataList, next = (string?)null });
+        }
+
+        [HttpGet("reacted")]
+        public async Task<IActionResult> GetReactedPostList([FromHeader(Name = "userId")] string userId, [FromQuery] string? next)
+        {
+            // Encrypt the Key if exists
+            if (!string.IsNullOrWhiteSpace(next))
+                next = _encryptionService.Decrypt(next);
+
+            const int pageSize = 15 + 1;
+            ServiceResponse<PostResponseDTO> res = await _postService.GetReactedPostListAsync(userId, pageSize, next);
+
+            if (!res.IsValid)
+                return HandleServiceError(res);
+
+            if (res.DataList?.Count >= pageSize)
+            {
+                var lastPost = res.DataList[^1];
+                res.DataList.RemoveAt(res.DataList.Count - 1);
+                string nextPostIdEncrpted = _encryptionService.Encrypt(lastPost.PostId);
+                return Ok(new { data = res.DataList, next = nextPostIdEncrpted });
+            }
+
+            return Ok(new { data = res.DataList, next = (string?)null });
+
+
+            return null!;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost([FromForm] PostDTO postDto, [FromHeader(Name = "userId")] string userId)
+        {
+            ServiceResponse<PostResponseDTO> res = await _postService.AddPostAsync(userId, postDto);
+            if (!res.IsValid)
+            {
+                return HandleServiceError(res);
+            }
+
+            return CreatedAtAction("Create", new { res.DataItem });
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePost([FromForm] PostDTO postDto, [FromHeader(Name = "userId")] string userId)
+        {
+            ServiceResponse<PostResponseDTO> res = await _postService.UpdatePostAsync(userId, postDto);
+            if (!res.IsValid)
+            {
+                return HandleServiceError(res);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePost([FromBody] string postId, [FromHeader(Name = "userId")] string userId)
+        {
+            string message;
+            bool res = await _postService.DeletePostAsync(userId ,postId, out message);
+            if (!res)
+                return BadRequest(new {Errors = new List<string>() { message } });
+            
+            return NoContent();
+        }
+        
+
+        // Utilities
+        private ActionResult HandleServiceError(ServiceResponse<PostResponseDTO> res)
+        {
+            return res.ErrorType switch
+            {
+                ErrorType.NotFound => NotFound(new { errors = res.Errors }),
+                ErrorType.BadRequest => BadRequest(new { errors = res.Errors }),
+                ErrorType.UnAuthorized => Unauthorized(new { errors = res.Errors }),
+                _ => BadRequest(new { errors = res.Errors }),
+            };
+        }
+    }
+}
---
+++ post-service/PostService/Presentation/Program.cs
@@ -0,0 +1,29 @@
+using Domain.IRepository;
+using Infrastructure.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Injection
+builder.Services.AddSingleton<IPostRepository, PostRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
