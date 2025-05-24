46940575fbe9e5e326b7f5021ed7397cea921a13
alyaa999 <alyaamamoon999@gmail.com>
2025-05-24 06:12:10 +0300

    Revert "-setup project Layers"
    This reverts commit a915dad7c29dbb368afb9b37320ad890b5c27fa7.

--- react-service/react-service/react-service.Infrastructure/Mongodb/MongoDbSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace react_service.Infrastructure.Mongodb
-{
-    public class MongoDbSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-    }
-
-}
--- react-service/react-service/reat-service.Application/DTO/ReactionPost/Request/CreateReactionRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace reat_service.Application.DTO.ReactionPost.Request
-{
-    public class CreateReactionRequest
-    {
-        public string PostId { get; set; }  
-        public string ReactionType { get; set; }    
-
-    }
-}
