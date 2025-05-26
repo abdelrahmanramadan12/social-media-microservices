09e82d5789a6d7ecdbd08ec9401ae935fb8b50cc
miinamaaher1 <minamaher1515@gmail.com>
2025-05-27 00:26:37 +0300

    [Setup] Add Entities

---
+++ feed-service/FeedService/Domain/Entities/Feed.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace Domain.Entities
+{
+    public class Feed
+    {
+        public ObjectId Id { get; set; }
+        public string UserId { get; set; }
+        public ICollection<Post> Timeline { get; set; }
+    }
+}
---
+++ feed-service/FeedService/Domain/Entities/Post.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+
+namespace Domain.Entities
+{
+    public enum Privacy
+    {
+        Public,
+        Private,
+        OnlyMe
+    }
+
+    public class Post
+    {
+        public ObjectId PostId { get; set; }
+        public string Content { get; set; }
+        public ICollection<MediaItem> MediaList { get; set; }
+        public int ReactsCount { get; set; }
+        public int CommentsCount { get; set; }
+        public bool IsEdited { get; set; }
+        public bool IsLiked { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public AuthorProfile AuthorProfile { get; set; }
+        public Privacy Privacy { get; set; }
+    }
+}
