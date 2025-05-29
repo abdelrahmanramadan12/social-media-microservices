9682cbb712fbb629227e6ca4a0bb908c205fbf9a
abdelrahmanramadan12 <abdelrahmanramadan416@gmail.com>
2025-05-29 16:21:04 +0300

    [Init] Initialized & Created database

---
+++ profile-service/profile-service/Domain/Entities/Profile.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Entities
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Email { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+
+        public string? ProfileUrl { get; set; }
+        public string? CoverUrl { get; set; }
+        public string? Address { get; set; }
+
+        public DateTime BirthDate { get; set; }
+        public string? MobileNo { get; set; }
+
+        public string? Bio { get; set; }
+
+        public int NoOfFollowers { get; set; }
+        public int NoOfFollowing { get; set; }
+    }
+}
---
+++ profile-service/profile-service/Infrastructure/Data/ProfileDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProfileDbContext : DbContext
+    {
+        public ProfileDbContext(DbContextOptions<ProfileDbContext> options)
+           : base(options) { }
+
+        public DbSet<Profile> Profiles { get; set; }
+
+    }
+}
