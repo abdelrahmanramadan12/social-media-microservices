c7a181067fec4bce8f2ae0570ab7f500c18ff63c
miinamaaher1 <minamaher1515@gmail.com>
2025-05-29 15:34:14 +0300

    [Fix] Move events to new folders

---
+++ feed-service/FeedService/Application/Events/CommentEvent.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using MongoDB.Bson;
+
+namespace Application.Events
+{
+    public class CommentEvent
+    {
+        public EventType EventType { get; set; }
+        public ObjectId CommentId { get; set; }
+        public string TextContent { get; set; }
+        public MediaItem MediaItem { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string AuthorId { get; set; }
+        public ObjectId PostId { get; set; }
+        public string PostAuthorId { get; set; }
+    }
+}
