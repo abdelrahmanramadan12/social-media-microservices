76a31221d9b209a01803e1df2e0d5bd5c1179484
miinamaaher1 <minamaher1515@gmail.com>
2025-06-01 23:19:46 +0300

    [Setup] define interfaces

---
+++ follow-aggregate-service/followAggregateService/Application/Abstractions/IFollowQueryService.cs
@@ -0,0 +1,10 @@
+﻿using Application.DTOs;
+
+namespace Application.Abstractions
+{
+    public interface IFollowQueryService
+    {
+        Task<FollowProfilesPageDTO> ListFollowingProfilesPage(string userId, string? cursor);
+        Task<FollowProfilesPageDTO> ListFollowersProfilesPage(string userId, string? cursor);
+    }
+}
