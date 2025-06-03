4a70c4dd73fb54046ee8d4eba8367b9c3f60dbdd
miinamaaher1 <minamaher1515@gmail.com>
2025-06-04 00:34:26 +0300

    [Feature] regesterd db

--- chat-service/ChatService/Web/Program.cs
+++ chat-service/ChatService/Web/Program.cs
@@ -1,3 +1,5 @@
+using MongoDB.Driver;
+using Scalar.AspNetCore;
 
 namespace Web
 {
@@ -13,12 +15,19 @@ namespace Web
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            builder.Services.AddSingleton<IMongoClient>(sp =>
+                new MongoClient(builder.Configuration.GetConnectionString("AtlasUri")));
+            builder.Services.AddSingleton(sp =>
+                sp.GetRequiredService<IMongoClient>().GetDatabase(builder.Configuration.GetSection("DatabaseName").Value));
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
