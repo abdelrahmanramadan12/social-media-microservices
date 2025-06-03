8bc6250ae7a90cfe9724b1da22e6fdc8fb49103b
miinamaaher1 <minamaher1515@gmail.com>
2025-06-04 00:33:07 +0300

    [Setup] defined dtos

---
+++ chat-service/ChatService/Application/DTOs/ConversationMessagesDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs
+{
+    public class ConversationMessagesDTO
+    {
+        public object Messages { get; internal set; }
+        public string Next { get; internal set; }
+    }
+}
---
+++ chat-service/ChatService/Application/DTOs/UserConversationsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs
+{
+    public class UserConversationsDTO
+    {
+        public object Conversations { get; internal set; }
+        public string Next { get; internal set; }
+    }
+}
