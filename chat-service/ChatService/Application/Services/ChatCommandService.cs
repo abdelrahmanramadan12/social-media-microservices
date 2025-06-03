49646663f1ec22b656331fb24e785a2fe475a8b6
miinamaaher1 <minamaher1515@gmail.com>
2025-06-04 00:34:08 +0300

    [Feat] implemented some interfaces

---
+++ chat-service/ChatService/Application/Services/ChatCommandService.cs
@@ -0,0 +1,35 @@
+﻿using Application.Abstractions;
+using Application.DTOs;
+
+namespace Application.Services
+{
+    public class ChatCommandService : IChatCommandService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ChatCommandService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task CreateConversationAsync(NewConversationDTO conversation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMessageAsync(MessageDTO message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditMessageAsync(MessageDTO message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMessageAsync(MessageDTO message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ chat-service/ChatService/Application/Services/ChatQueryService.cs
@@ -0,0 +1,55 @@
+﻿using Application.Abstractions;
+using Application.DTOs;
+
+namespace Application.Services
+{
+    public class ChatQueryService : IChatQueryService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ChatQueryService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ConversationMessagesDTO> GetConversationMessagesAsync(string conversationId, string? next)
+        {
+            const int PageSize = 20;
+
+            var messagesPlusOne = (await _unitOfWork.Messages.GetMessagesPageAsync(
+                conversationId,
+                next,
+                PageSize + 1
+            )).ToList();
+
+            var messages = messagesPlusOne.Take(PageSize).ToList();
+            next = messagesPlusOne.Count > PageSize ? messagesPlusOne[PageSize].Id : null;
+
+            return new ConversationMessagesDTO
+            {
+                Messages = messages,
+                Next = next
+            };
+        }
+
+        public async Task<UserConversationsDTO> GetUserConversationsAsync(string userId, string? next)
+        {
+            const int PageSize = 20;
+
+            var conversationsPlusOne = (await _unitOfWork.Conversations.GetConversationsPageAsync(
+                userId,
+                next,
+                PageSize + 1
+            )).ToList();
+
+            var conversations = conversationsPlusOne.Take(PageSize).ToList();
+            next = conversationsPlusOne.Count > PageSize ? conversationsPlusOne[PageSize].Id : null;
+
+            return new UserConversationsDTO
+            {
+                Conversations = conversations,
+                Next = next
+            };
+        }
+    }
+}
