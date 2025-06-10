613fe37fda273462764e5294f4dcbc9658a470fe
Mahmoud R. Keshk <mahmoudredakeshk@gmail.com>
2025-06-10 16:39:12 +0300

    Enhance controllers and project structure
    - Added error handling and pagination methods in BaseController.
    - Created PublicCommentController for managing public comments.
    - Updated Presentation.csproj to remove unnecessary package and add project reference.
    - Modified Presentation.csproj.user to change debug profile and add scaffolding properties.
    - Refined Program.cs to register HTTP clients and simplify startup configuration.

--- comment-aggregation-service/CommentAggregation/Presentation/Program.cs
+++ comment-aggregation-service/CommentAggregation/Presentation/Program.cs
@@ -1,18 +1,25 @@
+using Application.Services.Implementation;
+using Application.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+
+// Register HttpClient and ReactionServiceClient
+builder.Services.AddHttpClient<IReactionServiceClient, ReactionServiceClient>();
+builder.Services.AddHttpClient<IProfileServiceClient, ProfileServiceClient>();
+builder.Services.AddHttpClient<ICommentServiceClient, CommentServiceClient>();
+builder.Services.AddScoped<ICommentAggregationService, CommentAggregationService>();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
 
 app.UseHttpsRedirection();
 
