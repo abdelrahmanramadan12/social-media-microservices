b65d7b51fa3be913ca2bcc4673007f3802bcbf57
miinamaaher1 <minamaher1515@gmail.com>
2025-06-10 04:45:38 +0300

    Refactor event properties in PostEvent and CommentEvent
    - Replaced `CreatedAt` with `Timestamp` in both PostEvent.cs and CommentEvent.cs for consistent time representation.
    - Renamed `Id` to `CommentId` and `AuthorId` to `CommentAuthorId` in CommentEvent.cs for clarity.
    - Added `PostAuthorId` to CommentEvent.cs to identify the post's author.
    - Introduced `IsEdited` property in CommentEvent.cs to track comment edits.

--- comment-service/comment-service/Service/Events/PostEvent.cs
+++ comment-service/comment-service/Service/Events/PostEvent.cs
@@ -12,7 +12,7 @@ namespace Service.Events
         public Privacy Privacy { get; set; }
         public List<MediaDTO> Media { get; set; } = new List<MediaDTO>();
         public bool HasMedia => Media.Count > 0;
-        public DateTime CreatedAt { get; set; }
+        public DateTime Timestamp { get; set; }
         public bool IsEdited { get; set; }
         public int NumberOfLikes { get; set; }
         public int NumberOfComments { get; set; }
--- post-service/PostService/Application/Events/CommentEvent.cs
+++ post-service/PostService/Application/Events/CommentEvent.cs
@@ -2,11 +2,13 @@ namespace Application.Events
 {
     public class CommentEvent : QueueEvent
     {
-        public string Id { get; set; }
+        public string CommentId { get; set; }
         public string PostId { get; set; } = default!;
-        public string AuthorId { get; set; } = default!;
+        public string CommentAuthorId { get; set; } = default!;
         public string? Content { get; set; }
         public string? MediaUrl { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string PostAuthorId { get; set; } = default!;
+        public bool IsEdited { get; set; }
     }
 }
