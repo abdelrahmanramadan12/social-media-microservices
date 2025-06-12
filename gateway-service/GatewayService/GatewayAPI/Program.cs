8ea674a2bb993a8872b04ff581475c2e32cda74b
miinamaaher1 <minamaher1515@gmail.com>
2025-06-13 01:59:16 +0300

    Enable UserIdHeaderMiddleware in Program.cs

--- gateway-service/GatewayService/GatewayAPI/Program.cs
+++ gateway-service/GatewayService/GatewayAPI/Program.cs
@@ -29,7 +29,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-// app.UseMiddleware<UserIdHeaderMiddleware>();
+app.UseMiddleware<UserIdHeaderMiddleware>();
 
 app.UseHttpsRedirection();
 
