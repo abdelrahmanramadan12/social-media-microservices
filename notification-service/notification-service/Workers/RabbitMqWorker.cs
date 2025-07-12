a3a8763483d76fb65aa70e180e548fbb4b355c3a
miinamaaher1 <minamaher1515@gmail.com>
2025-07-13 01:39:35 +0300

    Remove IMessageListener from RabbitMqWorker; update initialization and listening methods accordingly

--- notification-service/notification-service/Workers/RabbitMqWorker.cs
+++ notification-service/notification-service/Workers/RabbitMqWorker.cs
@@ -5,20 +5,17 @@ namespace Workers
 {
     public class RabbitMqWorker(IFollowListener FollowListener,
         ICommentListener commentListener,
-        IMessageListener messageNotificationService,
         IReactionListener reactionListener
         ) : BackgroundService
     {
         private readonly IFollowListener _FollowListener = FollowListener;
         private readonly ICommentListener _CommentListener = commentListener;
-        private readonly IMessageListener _MessageNotificationService = messageNotificationService;
         private readonly IReactionListener _ReactionListener = reactionListener;
 
         public override async Task StartAsync(CancellationToken cancellationToken)
         {
             await _FollowListener.InitializeAsync();
             await _CommentListener.InitializeAsync();
-            await _MessageNotificationService.InitializeAsync();
             await _ReactionListener.InitializeAsync();
             await base.StartAsync(cancellationToken);
         }
@@ -27,7 +24,6 @@ namespace Workers
         {
             await _FollowListener.ListenAsync(stoppingToken);
             await _CommentListener.ListenAsync(stoppingToken);
-            await _MessageNotificationService.ListenAsync(stoppingToken);
             await _ReactionListener.ListenAsync(stoppingToken);
         }
 
@@ -35,7 +31,6 @@ namespace Workers
         {
             await _FollowListener.DisposeAsync();
             await _CommentListener.DisposeAsync();
-            await _MessageNotificationService.DisposeAsync();
             await _ReactionListener.DisposeAsync();
 
             await base.StopAsync(cancellationToken);
